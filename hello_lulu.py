87861f0be4eac7d6163ce648a1b7ee5e2f7dc6be
Alejandro J. Rojas <ale@trixandtrax.com>
2016-03-18 13:44:06 -0500

    update

--- hello_lulu.py
+++ hello_lulu.py
@@ -3,8 +3,8 @@ app_lulu = Flask(__name__)
 
 @app_lulu.route('/hello_lulu2')
 def hello_lulu():
-    return 'Hello World'
+    return 'Hello NYC'
 
 if __name__ == '__main__':
-    app_lulu.run()
+    app_lulu.run(debug=True)
 
